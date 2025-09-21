dad0940bba95c77df04a4be2665e599ca922289c
Ekaterina <grizhebovskayk@gmail.com>
2025-09-21 14:32:37 +0300

    Lab1 done

--- Lab1/Blue.cs
+++ Lab1/Blue.cs
@@ -1,4 +1,7 @@
-﻿namespace Lab1
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security;
+
+namespace Lab1
 {
     public class Blue
     {
@@ -7,7 +10,10 @@
             bool answer = false;
 
             // code here
-
+            if (a * b > 0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -17,7 +23,22 @@
             bool answer = false;
 
             // code here
-
+            int cd = (int)d;
+            double nd = d - cd;
+            if (nd > 0)
+            {
+                if (nd > 0.0001)
+                {
+                    answer = true;
+                }
+            }
+            if (nd < 0)
+            {
+                if (-nd > 0.0001)
+                {
+                    answer = true;
+                }
+            }
             // end
 
             return answer;
@@ -27,7 +48,10 @@
             bool answer = false;
 
             // code here
-
+            if (b != 0 && a % b == 0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -37,7 +61,21 @@
             double answer = 0;
 
             // code here
-
+            double absd = Math.Abs(d);
+            double absf = Math.Abs(f);
+            double absg = Math.Abs(g);
+            if (absd >= absf && absd >= absg)
+            {
+                answer = d;
+            }
+            if (absf > absd && absf >= absg)
+            {
+                answer = f;
+            }
+            if (absg > absd && absg > absf)
+            {
+                answer = g;
+            }
             // end
 
             return answer;
@@ -47,7 +85,18 @@
             double answer = 0;
 
             // code here
-
+            if (x <= -1.0)
+            {
+                answer = 0;
+            }
+            if (x > -1.0 && x <= 0.0)
+            {
+                answer = x + 1.0;
+            }
+            if (x > 0)
+            {
+                answer = 1;
+            }
             // end
 
             return answer;
@@ -57,7 +106,11 @@
             bool answer = false;
 
             // code here
-
+            double circleR = circleS / Math.PI;
+            if (squareS >= circleR * 4)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,17 +121,40 @@
             int answer = 0;
 
             // code here
+            if (Math.Abs(d) < Math.Abs(f))
+            { 
+                if (d > 0)
+                {
+                    answer = -1;
+                }
+            }
+            else
+            { 
+                if (f > 0)
+                {
+                    answer = 1;
+                }
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int a, int b, int c)
         {
             bool answer = false;
 
             // code here
-            
+            int ha = a / 2;
+            int hb = b / 2;
+            int hc = c / 2;
+            int sum = ha + hb + hc;
+            if (sum >= 2)
+            {
+                if (sum % 3 == 0 || (sum + 1) % 3 == 0)
+                {
+                    answer = true;
+                }
+            }
             // end
 
             return answer;
